e45cd74bc610028a40a4d4d67882097b60daae25
Nick Muntean <nrmuntean@gmail.com>
2025-06-07 13:35:22 -0500

    output PONG capability

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,39 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+  public static void main(String[] args) {
+        int port = 6379;
+        System.out.println("Logs from your program will appear here!");
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;
-    try {
-      serverSocket = new ServerSocket(port);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
-      serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            serverSocket.setReuseAddress(true);
+            try (Socket clientSocket = serverSocket.accept()) {
+                System.out.println("Server is running and has accepted a connection on port " + port + "!");
+
+                InputStream inputStream = clientSocket.getInputStream();
+                OutputStream outputStream = clientSocket.getOutputStream();
+
+                byte[] message = inputStream.readAllBytes();
+                String messageStr = new String(message);
+                
+                System.out.println(messageStr);
+
+                if (messageStr.equals("PING")) {
+                    System.out.println("Received EOF from client.");
+                    outputStream.write("+PONG\r\n".getBytes());
+                } else {
+                    System.out.println("Received message: " + message);
+                }
+
+              clientSocket.close();
+              serverSocket.close();
+          }
       } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
+          System.out.println("IOException: " + e.getMessage());
       }
-    }
   }
 }
